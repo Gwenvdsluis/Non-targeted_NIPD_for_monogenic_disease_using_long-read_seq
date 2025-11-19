31ac4e9fce781eed2e90e7ee83c53403ab62df6e
Gwenvdsluis <45999636+Gwenvdsluis@users.noreply.github.com>
2025-11-19 16:51:43 +0100

    Create wh_comp.py
    new file compare with whatshap

---
+++ wh_comp.py
@@ -0,0 +1,35 @@
+import subprocess
+import os
+
+sample = "2"
+folder = "/hpc/umc_laat/gvandersluis/data/"
+ph_vars = f"{folder}Ont_data_nhung/HG00{sample}/filtered_ROI/phased_ROI.vcf.gz"
+roi = f"{folder}Ref_HG/HG_annotation_ROI.bed"
+
+BM_ROI = f"{folder}Ont_data_nhung/HG00{sample}/filtered_ROI/ROI_eval.tsv"
+open(BM_ROI, "w").close()
+
+
+for region in open(roi, "r").readlines():
+    regspl = region.split("\t")
+    gen = regspl[3]
+    reg = regspl[0]+":"+regspl[1]+"-"+regspl[2]
+    out_vcf = f"{folder}Ont_data_nhung/HG00{sample}/filtered_ROI/{gen}_BM.vcf"
+    cmd = ["apptainer", "exec", "-B", "/hpc/:/hpc/", "/hpc/umc_laat/gvandersluis/software/bcftools_v1.9-1-deb_cv1.sif", "bcftools", "v$
+    subprocess.run(cmd, check=True)
+    tsv_out = f"{gen}_eval.tsv"
+    cmd2 = ["apptainer", "exec", "-B", "/hpc/:/hpc/", "/hpc/umc_laat/gvandersluis/software/whatshap_v1.sif", "whatshap", "compare", "-$
+    subprocess.run(cmd2, check=True)
+    if os.path.getsize(BM_ROI) == 0:
+        with open(BM_ROI, "w") as out:
+            with open(tsv_out, "r") as inp:
+                out.write(inp.read())
+    else:
+	with open(BM_ROI, "a") as out:
+            with open(tsv_out, "r") as inp:
+                next(inp)
+                out.write(inp.read())
+    os.remove(out_vcf)
+    os.remove(tsv_out)
+    print(gen, "\t Done")
+
